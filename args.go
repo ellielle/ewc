1f12f99afbb8d2c526d01aa8a11013c54e532644
ellielle <40385743+ellielle@users.noreply.github.com>
2024-08-19 11:58:19 -0400

    refactor to take piped text

--- args.go
+++ args.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 )
 
 func parseArgs() {
@@ -39,35 +41,83 @@ func parseArgs() {
 	// parse the flags so we can see what we're doing
 	flag.Parse()
 
-	// 0, 1 or multiple files
-	// it will read from stdin when no file is given
+	// flag.Arg(0) will return the filename, if any is provided
+	// otherwise, it will attempt to read from stdin when no file is given
 	files := flag.Args()
+
+	var text []string
 
 	// TODO: add support for multiple files in most functions, replace files[0] with loop
 
 	// TODO: add prettier help output than PrintDefaults()
 
-	if len(files) == 0 && files[0] == "" {
-		flag.PrintDefaults()
+	// 	f, err := os.Open(file)
+	// 	if err != nil {
+	// 		return 0, err
+	// 	}
+	// 	defer f.Close()
+	//
+	// 	// create a new scanner and scan each line
+	// 	// using a for loop
+	// 	// add the line count to the count variable
+	// 	scanner := bufio.NewScanner(f)
+	// 	count := 0
+	// 	for scanner.Scan() {
+	// 		count++
+	// 	}
+
+	if len(files) == 0 && flag.Arg(0) == "" {
+		// create a new scanner to read os.Stdin
+		// and check if we have piped data
+		fmt.Println("inside pipe code")
+		scanner := bufio.NewScanner(os.Stdin)
+
+		// if the text is piped in, read all of it into a []string
+		for scanner.Scan() {
+			text = append(text, scanner.Text())
+		}
+		if len(text) == 0 {
+			flag.PrintDefaults()
+			return
+		}
+		fmt.Println("byteCount here")
+
 		return
-	} else {
+	}
+
+	if len(files) != 0 && flag.Arg(0) != "" {
+		// open the given file and read it into a []string
+		// 		file, err := os.Open(flag.Arg(0))
+		// 		if err != nil {
+		// 			log.Fatal(err)
+		// 		}
+		// 		defer file.Close()
+		//
+		// 		scanner := bufio.NewScanner(file)
+		// 		for scanner.Scan() {
+		// 			text = append(text, scanner.Text())
+		// 		}
+
 		// get the line count, byte count, and word count
 		// and then output them with the file name
-		lc, err := lineCount(files[0])
+		lc, err := lineCount(flag.Arg(0))
 		if err != nil {
 			log.Fatal(err)
 		}
-		bc, err := byteCount(files[0])
+		bc, err := byteCount(flag.Arg(0))
 		if err != nil {
 			log.Fatal(err)
 		}
-		wc, err := wordCount(files[0])
+		wc, err := wordCount(flag.Arg(0))
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("%v %v %v %v\n", lc, wc, bc, files[0])
+		fmt.Printf("%v %v %v %v\n", lc, wc, bc, flag.Arg(0))
+		return
 	}
+
+	fmt.Println("going down to the flag calls")
 
 	if *help || *helpLong {
 		flag.PrintDefaults()
@@ -75,31 +125,31 @@ func parseArgs() {
 	}
 
 	if *bytes || *bytesLong {
-		bc, err := byteCount(files[0])
+		bc, err := byteCount(flag.Arg(0))
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("%v %v\n", bc, files[0])
+		fmt.Printf("%v %v\n", bc, flag.Arg(0))
 		return
 	}
 
 	if *chars || *charsLong {
-		cc, err := charCount(files[0])
+		cc, err := charCount(flag.Arg(0))
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("%v %v\n", cc, files[0])
+		fmt.Printf("%v %v\n", cc, flag.Arg(0))
 		return
 	}
 
 	if *lines || *linesLong {
-		lc, err := lineCount(files[0])
+		lc, err := lineCount(flag.Arg(0))
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%v %v\n", lc, files[0])
+		fmt.Printf("%v %v\n", lc, flag.Arg(0))
 		return
 	}
 
@@ -109,12 +159,12 @@ func parseArgs() {
 	}
 
 	if *words || *wordsLong {
-		wc, err := wordCount(files[0])
+		wc, err := wordCount(flag.Arg(0))
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("%v %v\n", wc, files[0])
+		fmt.Printf("%v %v\n", wc, flag.Arg(0))
 		return
 	}
 
